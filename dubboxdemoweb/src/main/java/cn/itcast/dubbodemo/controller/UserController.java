850ba3f916dfc63383e8fabc6ce2cd70c11a0b15
Myh <2366370293@qq.com>
2018-12-25 15:44:48 +0800

    首次提交代码

---
+++ dubboxdemoweb/src/main/java/cn/itcast/dubbodemo/controller/UserController.java
@@ -0,0 +1,21 @@
+package cn.itcast.dubbodemo.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import cn.itcast.dubbodemo.service.UserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Reference
+    private UserService userService;
+
+
+    @RequestMapping("showName")
+    @ResponseBody
+    public String showName(){
+        return userService.getName();
+    }
+}
